43d2a2cd4433f382f1157a85ec4dd2b89625b710
Alex Olson <alexolson87@gmail.com>
2019-05-12 19:09:05 -0700

    Added a basic file

---
+++ ECE508FinalProject_RainTracker.py
@@ -0,0 +1,33 @@
+#
+#    Authors: Alex Olson, Sahana Srinivasa Reddy
+#      Class: ECE 508 - Python Workshop
+#       Date: Spring 2019
+# Assignment: Final Project
+#
+# -----------------------------------------------------------------------------
+#                              RainTracker
+# -----------------------------------------------------------------------------
+# This program tracks incoming rain for a user-selected area. The user enters
+# a location to watch and RainTracker will tell them the probability of rain
+# over the next 24 hours. It also looks at 10-day forecasts to see if any 
+# storms are approaching the area. The application interacts with the user by
+# using a GUI. 
+#
+
+# Libraries
+import tkinter as tk
+
+# Main application
+def RainTracker():
+    print("\n\n\tStarting RainTracker...\n\n")
+    
+    
+    
+    
+    
+    
+    
+    print("\n\tClosing RainTracker...\n\n")
+
+# Run the program   
+RainTracker()
